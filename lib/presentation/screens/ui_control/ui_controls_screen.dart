79ad07dfee776ab102811addc2ba8f1a76913742
Anel <anelgabriel002@gmail.com>
2023-02-21 12:19:26 -0400

    first part done

---
+++ lib/presentation/screens/ui_control/ui_controls_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UIControlScreen extends StatelessWidget {
+  static const name = 'ui_control_screen';
+  const UIControlScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
